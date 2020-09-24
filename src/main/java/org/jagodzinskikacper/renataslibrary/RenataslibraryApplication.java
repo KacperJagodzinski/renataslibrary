da6767248d67da2b47d5ed952cda5489871b582b
KacperJagodzinski <kacper.jagodzinski@onet.eu>
2020-09-24 18:35:46 +0200

    Initial commit

---
+++ src/main/java/org/jagodzinskikacper/renataslibrary/RenataslibraryApplication.java
@@ -0,0 +1,13 @@
+package org.jagodzinskikacper.renataslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenataslibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenataslibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jagodzinskikacper/renataslibrary/RenataslibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jagodzinskikacper.renataslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenataslibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
